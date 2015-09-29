fe3387415cc9c8b21b53143383a5979fefaac71d
Grant Carthew <grant@carthew.net>
2015-09-29 16:05:49 +1000

    Working on _getLatestDir function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var uuid = require('node-uuid')
 var blobs = require('fs-blob-store')
 var path = require('path')
+var fs = require('fs')
 
 module.exports = BlobStore
 
@@ -7,6 +9,7 @@ function BlobStore(storeRoot, dirDepth) {
   if (!(this instanceof BlobStore)) { return new BlobStore(dir) }
   this.storeRoot = storeRoot
   this.dirDepth = depth
+  this.currentDirPath = []
   this.fsBlobStore = blobs(storePath)
 }
 
@@ -21,3 +24,34 @@ BlogStore.prototype.read((blobPath) => {
 BlobStore.prototype.delete((blobPath) => {
 
 })
+
+BlobStore.prototype._getLatestDir = (parentPath) => {
+  return new Promise((resolve, reject) => {
+
+
+
+    fs.readdir(parentPath, (err, files) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(files)
+      }
+    })
+
+
+
+  })
+}
+
+BlobStore.prototype._createUuidDir = (path) => {
+  return new Promise((resolve, reject) => {
+    var dirName = uuid.v4();
+    fs.mkdir(path, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
