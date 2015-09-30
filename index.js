21babb6c8ca05226c12904fe492417d8a83cc221
Grant Carthew <grant@carthew.net>
2015-09-30 13:12:49 +1000

    _latestDir working

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var uuid = require('node-uuid')
 var blobs = require('fs-blob-store')
+var Promise = require('bluebird')
 var path = require('path')
 var fs = require('fs')
 
 module.exports = BlobStore
 
 function BlobStore(storeRoot, dirDepth) {
-  if (!(this instanceof BlobStore)) { return new BlobStore(storeRoot, dirDepth) }
+  if (!(this instanceof BlobStore)) {
+    return new BlobStore(storeRoot, dirDepth)
+  }
   this.storeRoot = storeRoot
   this.dirDepth = dirDepth
   this.currentDirPath = []
@@ -25,44 +28,108 @@ BlobStore.prototype.delete = (blobPath) => {
 
 }
 
-BlobStore.prototype._getLatestDir = (parentPath) => {
+BlobStore.prototype._latestDirBackup = (parentPath) => {
   return new Promise((resolve, reject) => {
+    fs.readdir(parentPath, function(err, files) {
+      if (err) {
+        return reject(err)
+      }
+      var dirs = []
+      var filePath
 
-
-
-    function getSubDirs(dir, cb) {
-    fs.readdir(dir, function(err, files) {
-        var dirs = [],
-        filePath,
-        checkDirectory = function(err, stat) {
-            if(stat.isDirectory()) {
-                dirs.push(files[i]);
-            }
-            if(i + 1 === l) { // last record
-                cb(dirs);
-            }
-        };
-
-        for(var i=0, l=files.length; i<l; i++) {
-            if(files[i][0] !== '.') { // ignore hidden
-                filePath = dir+'/'+files[i];
-                fs.stat(filePath, checkDirectory);
-            }
-        }
-    });
-}
-
-
-
-  }).then((files) => {
-    console.dir(files);
-    files.map((file) => {
-      return fs.stat(file)
+      files.forEach((file, index) => {
+        filePath = path.join(parentPath, file)
+        fs.stat(filePath, (err, stat) => {
+          if (stat.isDirectory()) {
+            dirs.push({
+              name: file,
+              created: stat.birthtime.getTime()
+            })
+          }
+          if (index === files.length - 1) {
+            dirs.sort((a, b) => {
+              return parseInt(b.created) - parseInt(a.created)
+            })
+            resolve(dirs[0].name)
+          }
+        })
+      })
     })
   })
 }
 
-BlobStore.prototype._createUuidDir = (path) => {
+BlobStore.prototype._latestDir = function(parentPath) {
+  return new Promise((resolve, reject) => {
+    var self = this
+    fs.readdir(parentPath, function(err, fsItems) {
+      if (err) {
+        return reject(err)
+      }
+      resolve(self._filterDir(parentPath, fsItems))
+    })
+  }).then((dirs) => {
+    dirs.sort((a, b) => {
+      return parseInt(b.created) - parseInt(a.created)
+    })
+    return dirs[0].name
+  })
+}
+
+BlobStore.prototype._countDir = function(parentPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(parentPath, function(err, files) {
+      if (err) {
+        return reject(err)
+      }
+      var dirs = []
+      var filePath
+
+      files.forEach((file, index) => {
+        filePath = path.join(parentPath, file)
+        fs.stat(filePath, (err, stat) => {
+          if (stat.isDirectory()) {
+            dirs.push({
+              name: file,
+              created: stat.birthtime.getTime()
+            })
+          }
+          if (index === files.length - 1) {
+            dirs.sort((a, b) => {
+              return parseInt(b.created) - parseInt(a.created)
+            })
+            resolve(dirs[0].name)
+          }
+        })
+      })
+    })
+  })
+}
+
+BlobStore.prototype._filterDir = function(parentPath, itemArray) {
+  return new Promise((resolve, reject) => {
+    var self = this
+    var dirs = []
+    itemArray.forEach((item, index) => {
+      fullPath = path.join(parentPath, item)
+      fs.stat(fullPath, (err, stat) => {
+        if (err) {
+          return reject(err)
+        }
+        if (stat.isDirectory()) {
+          dirs.push({
+            name: item,
+            created: stat.birthtime.getTime()
+          })
+        }
+        if (index === itemArray.length - 1) {
+          resolve(dirs)
+        }
+      })
+    })
+  })
+}
+
+BlobStore.prototype._createUuidDir = function(path) {
   return new Promise((resolve, reject) => {
     var dirName = uuid.v4();
     fs.mkdir(path, (err) => {
