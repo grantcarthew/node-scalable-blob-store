61ea81ac01045c7259a26dfd7aba4be3d4bc7583
Grant Carthew <grant@carthew.net>
2015-09-30 16:12:38 +1000

    Refactored _latestDir to _dir, _fsItemInfo, _latestDir, _countDirs, _countFiles, _filterDirs, and _filterFiles

--- index.js
+++ index.js
@@ -28,116 +28,118 @@ BlobStore.prototype.delete = (blobPath) => {
 
 }
 
-BlobStore.prototype._latestDirBackup = (parentPath) => {
-  return new Promise((resolve, reject) => {
-    fs.readdir(parentPath, function(err, files) {
-      if (err) {
-        return reject(err)
-      }
-      var dirs = []
-      var filePath
+BlobStore.prototype._buildChildPath = function(parentPath) {
+  var workingDir = this.storeRoot
+  var childPath = ''
+  for (var i of this.dirDepth) {
+    var dir = this._latestDir(workingDir)
+    if (!dir) {
+      childPath = '/' + uuid.v4()
+    } else {
+      childPath = '/' + dir
+    }
+  }
+  return childPath
+}
 
-      files.forEach((file, index) => {
-        filePath = path.join(parentPath, file)
-        fs.stat(filePath, (err, stat) => {
-          if (stat.isDirectory()) {
-            dirs.push({
-              name: file,
-              created: stat.birthtime.getTime()
-            })
+BlobStore.prototype._nextChildPath = function(parentPath) {
+  var self = this
+  return self._latestDir(parentPath).then((dir) => {
+    if (!dir) {
+      return '/' + uuid.v4()
+    } else {
+      return '/' + dir
+    }
+  })
+  // var get = async(function*() {
+  // var left = yield readJSON('left.json')
+  // var right = yield readJSON('right.json')
+  // return {left: left, right: right}
+  // })
+}
+
+BlobStore.prototype._dir = function(parentPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(parentPath, function(err, fsItems) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          return []
+        } else {
+          return reject(err)
+        }
+      }
+      resolve(fsItems)
+    })
+  })
+}
+
+BlobStore.prototype._fsItemInfo = function(parentPath, fsItems) {
+  return new Promise((resolve, reject) => {
+    var stats = []
+    fsItems.forEach((item, index) => {
+      fullPath = path.join(parentPath, item)
+      fs.stat(fullPath, (err, stat) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            return
+          } else {
+            return reject(err)
           }
-          if (index === files.length - 1) {
-            dirs.sort((a, b) => {
-              return parseInt(b.created) - parseInt(a.created)
-            })
-            resolve(dirs[0].name)
-          }
+        }
+        stats.push({
+          name: item,
+          stat: stat
         })
+        if (index === fsItems.length - 1) {
+          resolve(stats)
+        }
       })
     })
   })
 }
 
 BlobStore.prototype._latestDir = function(parentPath) {
-  return new Promise((resolve, reject) => {
-    var self = this
-    fs.readdir(parentPath, function(err, fsItems) {
-      if (err) {
-        return reject(err)
-      }
-      resolve(self._filterDir(parentPath, fsItems))
-    })
+  var self = this
+  return self._dir(parentPath).then((fsItems) => {
+    return self._filterDirs(parentPath, fsItems)
   }).then((dirs) => {
     dirs.sort((a, b) => {
-      return parseInt(b.created) - parseInt(a.created)
+      return b.stat.birthtime.getTime() - a.stat.birthtime.getTime()
     })
     return dirs[0].name
   })
 }
 
-BlobStore.prototype._countDir = function(parentPath) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(parentPath, function(err, files) {
-      if (err) {
-        return reject(err)
-      }
-      var dirs = []
-      var filePath
+BlobStore.prototype._countDirs = function(parentPath) {
+  var self = this
+  return self._dir(parentPath).then((fsItems) => {
+    return self._filterDirs(parentPath, fsItems)
+  }).then((dirs) => {
+    return dirs.length
+  })
+}
 
-      files.forEach((file, index) => {
-        filePath = path.join(parentPath, file)
-        fs.stat(filePath, (err, stat) => {
-          if (stat.isDirectory()) {
-            dirs.push({
-              name: file,
-              created: stat.birthtime.getTime()
-            })
-          }
-          if (index === files.length - 1) {
-            dirs.sort((a, b) => {
-              return parseInt(b.created) - parseInt(a.created)
-            })
-            resolve(dirs[0].name)
-          }
-        })
-      })
+BlobStore.prototype._countFiles = function(parentPath) {
+  var self = this
+  return self._dir(parentPath).then((fsItems) => {
+    return self._filterDirs(parentPath, fsItems)
+  }).then((dirs) => {
+    return dirs.length
+  })
+}
+
+BlobStore.prototype._filterDirs = function(parentPath, fsItems) {
+  return this._fsItemInfo(parentPath, fsItems).then((fsItems) => {
+    return fsItems.filter((item) => {
+      return item.stat.isDirectory()
     })
   })
 }
 
-BlobStore.prototype._filterDir = function(parentPath, itemArray) {
-  return new Promise((resolve, reject) => {
-    var self = this
-    var dirs = []
-    itemArray.forEach((item, index) => {
-      fullPath = path.join(parentPath, item)
-      fs.stat(fullPath, (err, stat) => {
-        if (err) {
-          return reject(err)
-        }
-        if (stat.isDirectory()) {
-          dirs.push({
-            name: item,
-            created: stat.birthtime.getTime()
-          })
-        }
-        if (index === itemArray.length - 1) {
-          resolve(dirs)
-        }
-      })
-    })
-  })
-}
-
-BlobStore.prototype._createUuidDir = function(path) {
-  return new Promise((resolve, reject) => {
-    var dirName = uuid.v4();
-    fs.mkdir(path, (err) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve()
-      }
+BlobStore.prototype._filterFiles = function(parentPath, fsItems) {
+  return this._fsItemInfo(parentPath, fsItems).then((fsItems) => {
+    return fsItems.filter((item) => {
+      return !item.stat.isDirectory()
     })
   })
 }
