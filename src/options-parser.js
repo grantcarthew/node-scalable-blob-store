f795e7e82b65d5738ebd61d748fab2ff230937a9
Grant Carthew <grant@carthew.net>
2016-05-05 16:25:49 +1000

    Minor refactor

--- src/options-parser.js
+++ src/options-parser.js
@@ -1,32 +1,31 @@
 const mkdirp = require('mkdirp')
 
-module.exports = function (opts) {
-  if (!opts) {
-    throw new Error('scalable-blob-store options required.')
-  }
-
-  if (typeof opts !== 'object') {
+module.exports = function (options) {
+  if (!options || typeof options !== 'object') {
     throw new Error('scalable-blob-store options object required.')
   }
 
-  if (!opts.blobStoreRoot) {
-    throw new Error('The blobStoreRoot directory option must be set.')
+  if (!options.blobStoreRoot) {
+    throw new Error('The blobStoreRoot directory option is required.')
   }
 
-  if (!opts.idType) {
-    throw new Error('The idType option must be set.')
+  if (!options.idType) {
+    throw new Error('The idType option is required.')
   }
 
-  if (opts.idType.toUpperCase() !== 'uuid'.toUpperCase() &&
-      opts.idType.toUpperCase() !== 'cuid'.toUpperCase()) {
+  if (options.idType.toUpperCase() !== 'uuid'.toUpperCase() &&
+      options.idType.toUpperCase() !== 'cuid'.toUpperCase()) {
     throw new Error('The idType option is invalid.')
   }
 
-  mkdirp.sync(opts.blobStoreRoot)
-  if (opts.dirDepth < 1 || opts.dirDepth > 10) {
+  if (options.dirDepth < 1 || options.dirDepth > 10) {
     throw new Error('The dirDepth option must be between 1 and 10.')
   }
-  if (!opts.dirDepth) { opts.dirDepth = 3 }
-  if (!opts.dirWidth) { opts.dirWidth = 1000 }
-  return opts
+
+  options.dirDepth = options.dirDepth || 3
+  options.dirWidth = options.dirWidth || 1000
+
+  mkdirp.sync(options.blobStoreRoot)
+
+  return options
 }
