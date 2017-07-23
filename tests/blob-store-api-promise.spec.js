0bd7948f3790b8e40ec40ebadd2be6103a14f143
Grant Carthew <grant@carthew.net>
2017-07-23 10:25:52 +1000

    Removed mock-fs module

--- tests/blob-store-api-promise.spec.js
+++ tests/blob-store-api-promise.spec.js
@@ -1,10 +1,9 @@
+const os = require('os')
 const test = require('tape')
-const mock = require('mock-fs')
-const Promise = require('bluebird')
 const testBlobStore = require('./test-blob-store')
 
 const options = {
-  blobStoreRoot: '/tmp/blobs',
+  blobStoreRoot: os.tmpdir() + '/blobs',
   idType: 'cuid',
   dirDepth: 5,
   dirWidth: 10
@@ -12,13 +11,8 @@ const options = {
 
 module.exports = async function blobStoreApiPromiseSpec (opt) {
   test('blob-store api promise tests', async function (t) {
-    console.log('mocking')
-    mock({'tmp/blobs':{}})
-
     t.plan(12)
     await testBlobStore(t, options)
     t.pass('blob-store api promise tests completed')
-
-    mock.restore()
   })
 }
